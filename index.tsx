c3ccbb901e7b3dd462609d1a51969d899b88d99c
hussnaintechcreation <hussnaintechcreation@gmail.com>
2026-01-26 11:09:05 +0500

    project
    uploaded

---
+++ index.tsx
@@ -0,0 +1,26 @@
+
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import { SystemErrorBoundary, ApiKeyGuard } from './components/SystemGuard';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <SystemErrorBoundary>
+      <ApiKeyGuard>
+        <App />
+      </ApiKeyGuard>
+    </SystemErrorBoundary>
+  </React.StrictMode>
+);
